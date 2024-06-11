6b069454dc5085a954aac4433c054a8bfc07e843
Denis <bogusevich03@gmail.com>
2024-06-11 18:05:50 +0300

    Add is_power_of_two function and call in main.py

--- utils.py
+++ utils.py
@@ -12,3 +12,14 @@ def is_prime(n):
             return False
     return True
 
+<<<<<<< Updated upstream
+=======
+def is_power_of_five(n):
+    while n % 5 == 0 and n != 0:
+        n //= 5
+    return n == 1
+
+def is_power_of_two(n):
+    return (n & (n - 1)) == 0 and n != 0
+
+>>>>>>> Stashed changes
